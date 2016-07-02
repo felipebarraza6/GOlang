99af1d6c7c8bc04558af80e92e50b594d1e62e49
Felipe Barraza <felipe.barraza.vega@gmail.com>
2016-07-02 00:05:47 -0400

    Create variables.go

---
+++ introduccion_GO/variables.go
@@ -0,0 +1,21 @@
+//variables
+package main
+import "fmt"
+
+func main(){
+	//declarando variable  
+	var a string = "empezar"
+	fmt.Println(a)
+	//Declarando mas de una variable
+	var b, c, h int = 4, 5, 0
+	fmt.Println(b, c, h)
+	//Declarando variables con resultado booleano
+	var d = true
+	fmt.Println(d)
+	//Varibale e de tipo entero
+	var e int
+	fmt.Println(e)
+	//Declaracion de una cadena con otro tipo de sintaxis
+	f:= "short"
+	fmt.Println(f)
+}
