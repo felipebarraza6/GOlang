958dc89768f520b4386dbcfda13432505dc141fa
Felipe Barraza <felipe.barraza.vega@gmail.com>
2016-07-02 00:03:52 -0400

    Create variables.go

---
+++ introduccion_GO/variables.go
@@ -0,0 +1,15 @@
+package main
+import "fmt"
+func main(){
+	//Queremos imprimir cadenas golang
+	fmt.Println("go" + "lang")
+	//Queremos aprender a sumar/restar enteros
+	fmt.Println("5+5 =", 5+5)
+	//Queremos aprender a sumar flotantes
+	fmt.Println("7.0/3.0 =", 7.0/3.0)
+	//Manejo de booleanos
+	fmt.Println("resultado=")
+	fmt.Println(true && false)
+	fmt.Println(true || false)
+	fmt.Println(!false)
+}
